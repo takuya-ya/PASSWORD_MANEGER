c1f47263a0b5f7432eed96edc5954168aa1bf107
takuya <t.yamauchi251@gmail.com>
2024-06-29 21:53:21 +0900

    Exit、選択肢以外が入力された場合の処理を追加

--- password_manager.sh
+++ password_manager.sh
@@ -29,17 +29,21 @@ GetPassword()
 }
 
 choices=''
+echo 'パスワードマネージャーへようこそ!'
 while [ "$choices" != 'Exit' ]
 do
-    echo 'パスワードマネージャーへようこそ!'
     echo '次の選択肢から入力してください(Add Password/Get Password/Exit):'
     read choices
 if [ "$choices" == 'Add Password' ];then
     AddPassword
 elif [ "$choices" == 'Get Password' ];then
     GetPassword
-else
+elif [ "$choices" == 'Exit' ];then
     break
+else
+    echo '入力が間違えています。Add Password/Get Password/Exit から入力してください。'
+    echo
 fi
 done
+
 echo -e "Tahnk You\e[31m!\e[0m"
