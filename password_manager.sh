9ccd8b944d38428a27dcc64453d4fc848779fcae
takuya <t.yamauchi251@gmail.com>
2024-06-29 20:14:27 +0900

    AddPassword関数を追記

--- password_manager.sh
+++ password_manager.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+AddPassword()
+{
+    echo -n 'サービス名を入力してください:'
+    read service
+    echo -n 'ユーザー名を入力してください:'
+    read user
+    echo -n 'パスワードを入力してください:'
+    read -s password
+    echo
+
+    echo "サービス名：$service" >> passwords.sh
+    echo "ユーザー名：$user" >> passwords.sh
+    echo "パスワード：$password" >> passwords.sh
+}
+
 echo 'パスワードマネージャーへようこそ!'
-echo -n 'サービス名を入力してください:'
-read service
-echo -n 'ユーザー名を入力してください:'
-read user
-echo -n 'パスワードを入力してください:'
-read -s password
-echo
-
-echo "サービス名：$service" >> passwords.sh
-echo "ユーザー名：$user" >> passwords.sh
-echo "パスワード：$password" >> passwords.sh
-
+echo '次の選択肢から入力してください(Add Password/Get Password/Exit):'
+read choices
+if [ "$choices" == 'Add Password' ];then
+    AddPassword
+fi
 echo -e "Tahnk You\e[31m!\e[0m"
